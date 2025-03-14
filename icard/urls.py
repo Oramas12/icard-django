a94ef8c444edd8868d78c498039af988b840bfdd
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-14 17:42:47 -0400

    Update urls.py

--- icard/urls.py
+++ icard/urls.py
@@ -42,19 +42,21 @@ schema_view = get_schema_view(
     public=True,
 )
 
+# Página principal
+def home(request):
+    return JsonResponse({"message": "Welcome to iCard Django API!"})
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('docs/', schema_view.with_ui('swagger',
-         cache_timeout=0), name='schema-swagger-ui'),
-    path('redocs/', schema_view.with_ui('redoc',
-         cache_timeout=0), name='schema-redoc'),
-    path('api/', include('users.api.router')),
-    path('api/', include(router_user.urls)),
-    path('api/', include(router_category.urls)),
-    path('api/', include(router_product.urls)),
-    path('api/', include(router_table.urls)),
-    path('api/', include(router_orders.urls)),
-    path('api/', include(router_payments.urls))
+    path("", home),  # Agregar esta línea para manejar "/"
+    path("admin/", admin.site.urls),
+    path("docs/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redocs/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("api/", include(router_user.urls)),
+    path("api/", include(router_category.urls)),
+    path("api/", include(router_product.urls)),
+    path("api/", include(router_table.urls)),
+    path("api/", include(router_orders.urls)),
+    path("api/", include(router_payments.urls))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

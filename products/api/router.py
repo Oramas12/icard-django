3c721dbf26b2a55deb607abfb3243d4961822aba
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-17 15:09:28 -0400

    Update router.py

--- products/api/router.py
+++ products/api/router.py
@@ -1,9 +1,12 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from categories.api.views import CategoryApiViewSet  # Cambia esto según el módulo
 
-from products.api.views import ProductApiViewSet
-
-router_product = DefaultRouter()
-
-router_product.register(
-    prefix='products', basename='products', viewset=ProductApiViewSet
+router_category = DefaultRouter()
+router_category.register(
+    prefix='categories', basename='categories', viewset=CategoryApiViewSet
 )
+
+urlpatterns = [
+    path('', include(router_category.urls)),  # Agregar esto
+]

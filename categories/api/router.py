e75b9e18833102c98e55ca3762740da8025ec643
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-17 16:04:30 -0400

    Update router.py

--- categories/api/router.py
+++ categories/api/router.py
@@ -1,8 +1,12 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from categories.api.views import CategoryApiViewSet
+from categories.api.views import CategoryApiViewSet  # Asegúrate de que la vista existe
 
 router_category = DefaultRouter()
-
 router_category.register(
-    prefix='categories', basename='categories', viewset=CategoryApiViewSet
+    prefix="categories", basename="categories", viewset=CategoryApiViewSet
 )
+
+urlpatterns = [
+    path("", include(router_category.urls)),  # Asegúrate de que esta línea existe
+]

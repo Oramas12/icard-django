e1a68c6bfd785ac00168f22ccb8324d10e1c8e65
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-17 15:57:26 -0400

    Update router.py

--- payments/api/router.py
+++ payments/api/router.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from categories.api.views import CategoryApiViewSet  # Cambia esto según el módulo
+from payments.api.views import PaymentApiViewSet  # Asegúrate de que esta vista existe
 
-router_category = DefaultRouter()
-router_category.register(
-    prefix='categories', basename='categories', viewset=CategoryApiViewSet
+router_payments = DefaultRouter()
+router_payments.register(
+    prefix='payments', basename='payments', viewset=PaymentApiViewSet
 )
 
 urlpatterns = [
-    path('', include(router_category.urls)),  # Agregar esto
+    path('', include(router_payments.urls)),
 ]

a992cbb1df716bfd0b6af42537bcf0b199c69993
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-17 15:38:01 -0400

    Update router.py

--- orders/api/router.py
+++ orders/api/router.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from categories.api.views import CategoryApiViewSet  # Cambia esto según el módulo
+from orders.api.views import OrderApiViewSet  # Asegúrate de que esta vista existe
 
-router_category = DefaultRouter()
-router_category.register(
-    prefix='categories', basename='categories', viewset=CategoryApiViewSet
+router_orders = DefaultRouter()
+router_orders.register(
+    prefix='orders', basename='orders', viewset=OrderApiViewSet
 )
 
 urlpatterns = [
-    path('', include(router_category.urls)),  # Agregar esto
+    path('', include(router_orders.urls)),
 ]

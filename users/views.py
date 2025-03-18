4233650a967a623b3df2ec02dce66d4fe3a9199b
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-18 12:53:43 -0400

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.urls import path
+from .views import generate_password
 
-# Create your views here.
+urlpatterns = [
+    path('generate-password/', generate_password),  # Nueva ruta para generar la contraseña
+]

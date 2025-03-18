570bd71597843714b78fb2debe234f5507ec50bc
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-18 13:17:29 -0400

    Update views.py

--- users/api/views.py
+++ users/api/views.py
@@ -1,34 +1,10 @@
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.views import APIView
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
-from rest_framework.response import Response
+from django.http import JsonResponse
 from django.contrib.auth.hashers import make_password
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
-from users.models import User
-from users.api.serializers import UserSerializer
-
-
-class UserApiViewSet(ModelViewSet):
-    permission_classes = [IsAdminUser]
-    serializer_class = UserSerializer
-    queryset = User.objects.all()
-
-    def create(self, request, *args, **kwargs):
-        request.data['password'] = make_password(request.data['password'])
-        return super().create(request, *args, **kwargs)
-
-    def partial_update(self, request, *args, **kwargs):
-        password = request.data['password']
-        if password:
-            request.data['password'] = make_password(password)
-        else:
-            request.data['password'] = request.user.password
-        return super().update(request, *args, **kwargs)
-
-
-class UserView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        serializer = UserSerializer(request.user)
-        return Response(serializer.data)
+@api_view(['GET'])
+@permission_classes([AllowAny])  # Permite acceso sin autenticación
+def generate_password(request):
+    hashed_password = make_password('Admin123!')
+    return JsonResponse({'hashed_password': hashed_password})

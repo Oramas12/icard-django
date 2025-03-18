0205348273d4588aa084f9893fb2b901661f61c2
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-18 13:16:55 -0400

    Update router.py

--- users/api/router.py
+++ users/api/router.py
@@ -1,15 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import TokenObtainPairView
-from users.api.views import UserApiViewSet, UserView
-
-router_user = DefaultRouter()
-router_user.register(
-    prefix='users', basename='users', viewset=UserApiViewSet
-)
+from django.urls import path
+from .views import generate_password
 
 urlpatterns = [
-    path('', include(router_user.urls)),  # <-- Agregar esto para incluir el router
-    path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/me/', UserView.as_view()),
+    path('users/generate-password/', generate_password, name='generate-password'),
 ]

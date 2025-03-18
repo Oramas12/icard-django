e446ce2abdc2a2a989a5cfdd1db91f07bf25420e
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-18 10:43:07 -0400

    Update settings.py

--- icard/settings.py
+++ icard/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = 'django-insecure-wsp4v2oll3xdshszu89zf@6m9dn^va#gsin12+h4n*kr2w&-6z
 DEBUG = True
 
 # ALLOWED_HOSTS = ['127.0.0.1', "localhost"]
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    os.getenv("ALLOWED_HOSTS", "django-server-production-af4d.up.railway.app")
+]
 
 # Application definition
 

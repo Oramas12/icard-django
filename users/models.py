0e0ff98c20518636031407ef216a7a2d12a41c86
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-18 12:12:49 -0400

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class User(AbstractUser):
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+    class Meta:
+        db_table = 'auth_user'  # 🔥 Esto fuerza a Django a usar "auth_user"

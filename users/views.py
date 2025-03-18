c9d33efbc520af8d793afee7e7b1567e4422c04a
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-18 13:10:17 -0400

    Update views.py

--- users/views.py
+++ users/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
 @api_view(['GET'])
-@permission_classes([AllowAny])  # Permitir acceso sin autenticación
+@permission_classes([AllowAny])  # Permite acceso sin autenticación
 def generate_password(request):
     hashed_password = make_password('Admin123!')
     return JsonResponse({'hashed_password': hashed_password})

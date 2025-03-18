375b3767a35b6ac05166221da4c50e2400c725d4
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-18 12:30:29 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -2,18 +2,19 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import django
-from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icard.settings')
-django.setup()
-
-User = get_user_model()
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "Admin123!")
-    print("Superusuario creado: admin / Admin123!")
-
-
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'icard.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
     main()

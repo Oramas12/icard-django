56129c0817c864873072e0a817c374933371462c
Oramas12 <79879874+Oramas12@users.noreply.github.com>
2025-03-18 13:02:17 -0400

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,6 +1,10 @@
 from django.http import JsonResponse
 from django.contrib.auth.hashers import make_password
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
+@api_view(['GET'])
+@permission_classes([AllowAny])  # Permitir acceso sin autenticación
 def generate_password(request):
     hashed_password = make_password('Admin123!')
     return JsonResponse({'hashed_password': hashed_password})
